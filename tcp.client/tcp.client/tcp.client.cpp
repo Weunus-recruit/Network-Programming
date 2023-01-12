942b05d693dd3f61807ba39f7d3f255d71200202
wls4421 <76942710+wls4421@users.noreply.github.com>
2023-01-12 14:10:04 +0900

    1234

---
+++ tcp.client/tcp.client/tcp.client.cpp
@@ -0,0 +1,66 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+#include <WinSock2.h>
+
+#pragma comment(lib, "ws2_32")
+#pragma warning(disable : 4996)
+
+#define PORT		4578
+#define PACKET_SIZE	1024
+#define SERVER_IP	"192.168.0.162"
+
+using namespace std;
+
+int main(void)
+{
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	SOCKET hSocket;
+	hSocket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	SOCKADDR_IN tAddr = {};
+	tAddr.sin_family = AF_INET;
+	tAddr.sin_port = htons(PORT);
+	tAddr.sin_addr.s_addr = inet_addr(SERVER_IP);
+
+	connect(hSocket, (SOCKADDR*)&tAddr, sizeof(tAddr));
+
+	cout << "Server Connected" << endl;
+
+	cout << "Name : ";
+	char	cName[100];
+	cin >> cName;
+	strcat(cName, " Send : ");
+	char	cNameCopy[100];
+	strcpy(cNameCopy, cName);
+	send(hSocket, cName, strlen(cName), 0);
+	
+	char	cBuffer[PACKET_SIZE] = {};
+	char	cMsg[100];
+	
+	
+
+	while (cBuffer != "quit")
+	{
+		cout << "Input Message: ";
+		cin >> cMsg;
+		strcat(cName, cMsg);
+		send(hSocket, cName, strlen(cName), 0);
+		recv(hSocket, cBuffer, PACKET_SIZE, 0);
+		cout << "Server Send : " << cBuffer << endl;
+		strcpy(cName, cNameCopy);
+	}
+	cout << "Close Socket" << endl;
+	closesocket(hSocket);
+
+	WSACleanup();
+
+
+	return 0;
+}
---
+++ tcp.server/tcp.server/tcp.server.cpp
@@ -0,0 +1,71 @@
+﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "pch.h"
+#include <iostream>
+#include <stdio.h>
+#include <WinSock2.h>
+#include <time.h>
+
+#pragma comment(lib, "ws2_32")
+#pragma warning(disable : 4996)
+
+#define PORT		4578
+#define PACKET_SIZE	1024
+
+using namespace std;
+
+int main(void)
+{
+	WSADATA wsaData;
+	WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+	SOCKET hListen;
+	hListen = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+	SOCKADDR_IN tListenAddr = {};
+	tListenAddr.sin_family = AF_INET;
+	tListenAddr.sin_port = htons(PORT);
+	tListenAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+	bind(hListen, (SOCKADDR*)&tListenAddr, sizeof(tListenAddr));
+	listen(hListen, SOMAXCONN);
+
+	SOCKADDR_IN tCIntAddr = {};
+	int iCIntSize = sizeof(tCIntAddr);
+	SOCKET hClient = accept(hListen, (SOCKADDR*)&tCIntAddr, &iCIntSize);
+
+	char	sID[] = "Server";
+	char	sMsg[100];
+	char	cBuffer[PACKET_SIZE] = {};
+	int temp = 0;
+	cout << "Client Connected" << endl;
+
+	while (cBuffer != "quit")
+	{
+		cout << "Input Message: ";
+		cin >> sMsg ;
+		recv(hClient, cBuffer, PACKET_SIZE, 0);
+		if (temp == 0)
+		{
+			cout << cBuffer << "Connected" << endl;
+			temp += 1;
+		}
+		else
+		{
+			cout << cBuffer << endl;
+		}
+		send(hClient, sMsg, strlen(sMsg), 0);
+		
+
+	}
+
+	cout << "Close Listen" << endl;
+	
+	closesocket(hClient);
+	closesocket(hListen);
+
+	WSACleanup();
+
+	return 0;
+}
