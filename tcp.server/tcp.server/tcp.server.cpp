38d6fe1616e7767904470edd86c5db8c4b3364e3
wls4421 <76942710+wls4421@users.noreply.github.com>
2023-01-12 18:55:08 +0900

    412412

--- tcp.server/tcp.server/tcp.server.cpp
+++ tcp.server/tcp.server/tcp.server.cpp
@@ -1,11 +1,13 @@
 ﻿#define _WINSOCK_DEPRECATED_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS
 
+
 #include "pch.h"
 #include <iostream>
 #include <stdio.h>
 #include <WinSock2.h>
 #include <time.h>
+#include <thread>
 
 #pragma comment(lib, "ws2_32")
 #pragma warning(disable : 4996)
@@ -15,26 +17,76 @@
 
 using namespace std;
 
-int main(void)
+SOCKET skt, client_sock;
+
+void proc_recvs() {
+	char buffer[PACKET_SIZE] = { 0 };
+	char ClientID[PACKET_SIZE] = {};
+	ZeroMemory(&ClientID, PACKET_SIZE);
+	recv(client_sock, ClientID, PACKET_SIZE, 0);
+	char QuitMsg[] = "quit";
+	while (!WSAGetLastError()) {
+		ZeroMemory(&buffer, PACKET_SIZE);
+		recv(client_sock, buffer, PACKET_SIZE, 0);
+		cout << ClientID << "에게 받은 메시지: " << buffer << endl;
+		if (strcmp(QuitMsg, buffer) == 0)
+		{
+			cout << "Client Left" << endl;
+			cout << "Close Socket" << endl;
+
+			closesocket(client_sock);
+			closesocket(skt);
+			WSACleanup();
+			cout << "Insert quit" << endl;
+			break;
+
+		}
+	}
+}
+
+int main()
 {
-	WSADATA wsaData;
-	WSAStartup(MAKEWORD(2, 2), &wsaData);
+	WSADATA wsa;
+	WSAStartup(MAKEWORD(2, 2), &wsa);
 
-	SOCKET hListen;
-	hListen = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	skt = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
 
-	SOCKADDR_IN tListenAddr = {};
-	tListenAddr.sin_family = AF_INET;
-	tListenAddr.sin_port = htons(PORT);
-	tListenAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	SOCKADDR_IN addr = {};
+	addr.sin_family = AF_INET;
+	addr.sin_port = htons(PORT);
+	addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-	bind(hListen, (SOCKADDR*)&tListenAddr, sizeof(tListenAddr));
-	listen(hListen, SOMAXCONN);
+	bind(skt, (SOCKADDR*)&addr, sizeof(addr));
+	listen(skt, SOMAXCONN);
 
-	SOCKADDR_IN tCIntAddr = {};
-	int iCIntSize = sizeof(tCIntAddr);
-	SOCKET hClient = accept(hListen, (SOCKADDR*)&tCIntAddr, &iCIntSize);
+	SOCKADDR_IN client = {};
+	int client_size = sizeof(client);
+	ZeroMemory(&client, client_size);
+	client_sock = accept(skt, (SOCKADDR*)&client, &client_size);
 
+	char buffer[PACKET_SIZE] = { 0 };
+	thread proc2(proc_recvs);
+
+	cout << "Client Connected" << endl;
+
+	while (!WSAGetLastError()) {
+		cin >> buffer;
+		
+		if (strcmp(buffer, "quit") == 0)
+		{
+			send(client_sock, buffer, strlen(buffer), 0);
+			cout << "quit Access" << endl;
+			
+			return 0;
+		}
+		
+		send(client_sock, buffer, strlen(buffer), 0);
+	}
+	
+
+	proc2.join();
+
+	/*
 	char	sID[] = "Server";
 	char	cID[100] = {};
 	char	sMsg[100];
@@ -47,13 +99,14 @@ int main(void)
 	
 	strcpy(cID, cBuffer);
 	
-
+	
 	while ((strcmp(quitMsg, cBuffer) != 0) && (strcmp(sMsg,"quit") != 0))
 	{
 		
+		recv(hClient, cBuffer, sizeof(cBuffer) - 1, 0);
 		cout << "Input Message: ";
 		cin >> sMsg ;
-		recv(hClient, cBuffer, sizeof(cBuffer) - 1, 0);
+		send(hClient, sMsg, sizeof(sMsg) - 1, 0);
 		if (temp == 0)
 		{
 			cout << "Client Name : " << cID << endl;
@@ -65,17 +118,15 @@ int main(void)
 		{
 			cout << cBuffer << endl;
 		}
-		send(hClient, sMsg, sizeof(sMsg) -1, 0);
+		
 
 
 	}
-
+	*/
 	cout << "Close Listen" << endl;
 	
-	closesocket(hClient);
-	closesocket(hListen);
+	closesocket(client_sock);
+	closesocket(skt);
 
 	WSACleanup();
-
-	return 0;
 }
